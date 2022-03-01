99c357426d03f38b5b4ef38b8f3816695a527c6d
Tim Baumgärtner <baumgaertner.t@gmail.com>
2022-03-01 17:59:26 +0100

    first commit

---
+++ query/__init__.py
@@ -0,0 +1,45 @@
+import logging
+
+import azure.functions as func
+
+from square_skill_api.models.prediction import QueryOutput
+from square_skill_api.models.request import QueryRequest
+
+from square_skill_helpers.config import SquareSkillHelpersConfig
+from square_skill_helpers.square_api import ModelAPI
+
+logger = logging.getLogger(__name__)
+
+config = SquareSkillHelpersConfig.from_dotenv()
+model_api = ModelAPI(config)
+
+
+async def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    request_body = req.get_json()
+
+    query = request_body["query"]
+    context = request_body["skill_args"]["context"]
+    prepared_input = [context, query] 
+    
+    model_request = { 
+        "input": prepared_input,
+        "preprocessing_kwargs": {},
+        "model_kwargs": {},
+        "adapter_name": "AdapterHub/bert-base-uncased-pf-boolq"
+    }
+    model_api_output = await model_api(
+        model_name="bert-base-uncased", 
+        pipeline="sequence-classification", 
+        model_request=model_request
+    )
+    logger.info(f"Model API output:\n{model_api_output}")
+
+    response = QueryOutput.from_sequence_classification(
+        answers=["No", "Yes"], 
+        model_api_output=model_api_output, 
+        context=context
+    ).json()
+    
+    return func.HttpResponse(response, status_code=200)
+
